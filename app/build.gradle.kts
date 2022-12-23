2ab2f4cc003c14519f528e3710df01245d6c2ef9
Mahdi Parastesh <fulcrum1378@gmail.com>
2022-12-23 20:28:42 +0330

    2022.12.23 - 1 (implemented backup buttons and the first backup action)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "7.4.0"
+        versionName = "7.4.6"
     }
 
     setFlavorDimensions(listOf("calendar"))
