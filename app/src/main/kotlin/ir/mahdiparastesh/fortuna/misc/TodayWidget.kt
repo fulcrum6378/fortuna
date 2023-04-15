251dc69c9ef1a6b5541a702217bda62ea22a7802
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-04-15 07:42:12 +0330

    2023.04.15 - 3 (TodayWidget improved)

--- app/src/main/kotlin/ir/mahdiparastesh/fortuna/misc/TodayWidget.kt
+++ app/src/main/kotlin/ir/mahdiparastesh/fortuna/misc/TodayWidget.kt
@@ -4,6 +4,7 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Context
+import android.content.res.Configuration
 import android.icu.util.Calendar
 import android.os.Bundle
 import android.widget.RemoteViews
@@ -21,26 +22,12 @@ class TodayWidget : AppWidgetProvider() {
         ids.forEach { id -> manager.updateAppWidget(id, update(c)) }
     }
 
-    /*override fun onAppWidgetOptionsChanged(
+    override fun onAppWidgetOptionsChanged(
         c: Context, manager: AppWidgetManager, id: Int, newOptions: Bundle
     ) {
         super.onAppWidgetOptionsChanged(c, manager, id, newOptions)
-        manager.updateAppWidget(id, update(c).apply {
-            setImageViewBitmap(
-                R.id.bg, MaterialShapeDrawable(
-                    ShapeAppearanceModel.Builder().setAllCorners(
-                        CornerFamily.CUT, c.resources.getDimension(R.dimen.smallCornerSize)
-                    ).build()
-                ).let {
-                    it.fillColor = c.resources.getColorStateList(R.color.todayWidget, null)
-                    val den = c.resources.displayMetrics.density
-                    it.toBitmap(
-                        (den * newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH)).toInt(),
-                        (den * newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT)).toInt()
-                    )
-                })
-        })
-    }*/
+        manager.updateAppWidget(id, update(c))
+    }
 
     companion object {
         fun update(c: Context) = RemoteViews(c.packageName, R.layout.today_widget).apply {
@@ -51,7 +38,11 @@ class TodayWidget : AppWidgetProvider() {
                     ShapeAppearanceModel.Builder().setAllCorners(CornerFamily.CUT, den * 4f).build()
                 ).let {
                     it.fillColor = c.resources.getColorStateList(R.color.todayWidget, null)
-                    it.toBitmap((den * 30).toInt(), (den * 50).toInt())
+                    c.resources.configuration.orientation
+                    val size = arrayOf(30, 50)
+                    if (c.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE)
+                        size.reverse()
+                    it.toBitmap((den * size[0]).toInt(), (den * size[1]).toInt())
                 })
             setOnClickPendingIntent(R.id.root, Kit.openInDate(c, cal, 1))
             setTextViewText(
