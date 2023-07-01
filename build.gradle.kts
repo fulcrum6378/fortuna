4dd79f5720bd389b57e5c3a484708778de28bef4
Mahdi Parastesh <fulcrum1378@gmail.com>
2023-07-01 21:42:45 +0330

    2023.07.01 - 4 (runConfigurations)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-    id("com.android.application") version("8.0.2") apply(false)
-    id("org.jetbrains.kotlin.android") version("1.8.20") apply(false)
+    id("com.android.application") version ("8.0.2") apply (false)
+    id("org.jetbrains.kotlin.android") version ("1.8.20") apply (false)
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
-    delete "${rootDir}/app/build/"
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir, "${rootDir}/app/build/")
 }
