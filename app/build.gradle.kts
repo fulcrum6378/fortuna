98b4f71d237661ebb63e0cbb8e8014c7cddd22ff
Mahdi Parastesh <fulcrum1378@gmail.com>
2024-05-09 14:19:01 +0330

    2024.05.09 - 1 (modernisation)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,14 +3,14 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
     namespace = "ir.mahdiparastesh.fortuna"
     compileSdk = 34
-    buildToolsVersion = "35.0.0-rc2"
+    buildToolsVersion = "35.0.0-rc3"
 
     defaultConfig {
         applicationId = "ir.mahdiparastesh.fortuna"
         minSdk = 26
         targetSdk = 34
         versionCode = 12
-        versionName = "11.4.0"
+        versionName = "11.4.1"
     }
 
     sourceSets.getByName("main") {
@@ -25,13 +25,12 @@ android {
     }
     sourceSets.getByName("iranian") { res.setSrcDirs(listOf("src/res", "src/res_iranian")) }
     sourceSets.getByName("gregorian") { res.setSrcDirs(listOf("src/res", "src/res_gregorian")) }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21; targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions { jvmTarget = "21" }
-    buildFeatures { buildConfig = true; viewBinding = true }
 
+    buildFeatures { buildConfig = true; viewBinding = true }
     signingConfigs {
         create("release") {
             storeFile = file(System.getenv("JKS_PATH"))
@@ -53,7 +52,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.google.android.material:material:1.12.0")
 }
