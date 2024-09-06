5c5e218e47f814a665acdcb86c2666ef958bfc9c
Mahdi Parastesh <fulcrum1378@gmail.com>
2024-09-06 16:40:57 +0330

    2024.09.06 - 1 (ANDROID 15 & other modernisation)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version ("8.4.2") apply (false)
-    id("org.jetbrains.kotlin.android") version ("1.9.23") apply (false)
+    id("com.android.application") version ("8.5.2") apply (false)
+    id("org.jetbrains.kotlin.android") version ("2.0.20") apply (false)
 }
 
 tasks.register("clean", Delete::class) {
