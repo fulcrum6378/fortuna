72ac1a9f63417f3c8e62284a5ab88f8ad6774cd2
Mahdi Parastesh <fulcrum1378@gmail.com>
2025-06-10 12:55:05 +0330

    2025.06.10 - 1: luna and annus were built in compose

--- android-compose/src/kotlin/ir/mahdiparastesh/fortuna/Main.kt
+++ android-compose/src/kotlin/ir/mahdiparastesh/fortuna/Main.kt
@@ -4,42 +4,61 @@ package ir.mahdiparastesh.fortuna
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
+import java.time.temporal.ChronoField
 
 class Main : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,6 +67,9 @@ class Main : ComponentActivity() {
         setContent { FortunaTheme { Root() } }
     }
 }
+
+@get:Composable
+val c: Fortuna get() = (LocalActivity.current as Main).applicationContext as Fortuna
 
 @Composable
 fun Root() {
@@ -60,6 +82,30 @@ fun Root() {
                 drawerContainerColor = MaterialTheme.colorScheme.primary,
                 drawerContentColor = MaterialTheme.colorScheme.onPrimary,
             ) {
+
+                @Composable
+                fun NavItem(
+                    @StringRes title: Int,
+                    @DrawableRes icon: Int,
+                ) {
+                    NavigationDrawerItem(
+                        label = {
+                            Text(
+                                text = stringResource(title),
+                                color = MaterialTheme.colorScheme.onPrimary,
+                            )
+                        },
+                        selected = false,
+                        onClick = { /*TODO*/ },
+                        icon = {
+                            Icon(
+                                painterResource(icon),
+                                contentDescription = stringResource(title),
+                                tint = MaterialTheme.colorScheme.onPrimary,
+                            )
+                        },
+                    )
+                }
 
                 @Composable
                 fun NavDivider() {
@@ -85,12 +131,9 @@ fun Root() {
         }
     ) {
         Scaffold(
+            modifier = Modifier.fillMaxSize(),
             topBar = {
                 TopAppBar(
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primary,
-                        titleContentColor = MaterialTheme.colorScheme.onPrimary,
-                    ),
                     title = { Text(stringResource(R.string.app_name)) },
                     navigationIcon = {
                         IconButton(onClick = { scope.launch { drawerState.open() } }) {
@@ -109,73 +152,102 @@ fun Root() {
                                 tint = MaterialTheme.colorScheme.onPrimary,
                             )
                         }
-                    }
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    ),
                 )
             },
-            modifier = Modifier.fillMaxSize()
         ) { innerPadding ->
-            Panel(modifier = Modifier.padding(innerPadding))
+            Column(modifier = Modifier.padding(innerPadding)) {
+                Panel()
+                //Grid()
+            }
         }
     }
 }
 
 @Composable
-fun NavItem(
-    @StringRes title: Int,
-    @DrawableRes icon: Int,
-) {
-    NavigationDrawerItem(
-        icon = {
-            Icon(
-                painterResource(icon),
-                contentDescription = stringResource(title),
-                tint = MaterialTheme.colorScheme.onPrimary,
-            )
-        },
-        label = {
-            Text(
-                text = stringResource(title),
-                color = MaterialTheme.colorScheme.onPrimary,
-            )
-        },
-        selected = false,
-        onClick = { /*TODO*/ }
-    )
-}
+fun Panel() {
+    val c = c
+    val months = stringArrayResource(R.array.luna)
+    var selectedLuna by rememberSaveable {
+        mutableStateOf(months[c.date[ChronoField.MONTH_OF_YEAR] - 1])
+    }
+    var lunaExpanded by rememberSaveable { mutableStateOf(false) }
+    var annus by rememberSaveable { mutableStateOf(c.date[ChronoField.YEAR].toString()) }
 
-@Composable
-fun Panel(modifier: Modifier) {
-    val options = listOf("Option 1", "Option 2", "Option 3")
-    var selectedOption = remember { mutableStateOf(options[0]) }
-    var expanded = remember { mutableStateOf(false) }
-
-    ExposedDropdownMenuBox(
-        modifier = modifier,
-        expanded = expanded.value,
-        onExpandedChange = { expanded.value = !expanded.value }
-    ) {
-        TextField(
-            value = selectedOption.value,
-            onValueChange = {},
-            readOnly = true,
-            trailingIcon = {
-                Icon(Icons.Default.ArrowDropDown, contentDescription = "Dropdown Icon")
-            },
-            //modifier = Modifier.menuAnchor(type, enabled)
-        )
-        ExposedDropdownMenu(
-            expanded = expanded.value,
-            onDismissRequest = { expanded.value = false }
-        ) {
-            options.forEach { option ->
-                DropdownMenuItem(
-                    text = { Text(option) },
-                    onClick = {
-                        selectedOption.value = option
-                        expanded.value = false
-                    }
+    Box {
+        Box(  // a shadow for the TopAppBar
+            Modifier
+                .fillMaxWidth()
+                .height(4.dp)
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(Color(0x40000000), Color.Transparent),
+                        startY = 0f,
+                        endY = Float.POSITIVE_INFINITY
+                    )
                 )
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 20.dp),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            val textFieldColours = TextFieldDefaults.colors(
+                unfocusedContainerColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent
+            )
+
+            // month selector (luna)
+            ExposedDropdownMenuBox(
+                expanded = lunaExpanded,
+                onExpandedChange = { lunaExpanded = it },
+                modifier = Modifier.width(200.dp),
+            ) {
+                TextField(
+                    value = selectedLuna,
+                    onValueChange = {},
+                    modifier = Modifier
+                        .menuAnchor(MenuAnchorType.PrimaryNotEditable)
+                        .width(200.dp),
+                    readOnly = true,
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(lunaExpanded)
+                    },
+                    colors = textFieldColours,
+                )
+                ExposedDropdownMenu(
+                    expanded = lunaExpanded,
+                    onDismissRequest = { lunaExpanded = false },
+                ) {
+                    months.forEach { option ->
+                        DropdownMenuItem(
+                            text = { Text(option) },
+                            onClick = {
+                                selectedLuna = option
+                                lunaExpanded = false
+                            }
+                        )
+                    }
+                }
             }
+
+            // year selector (annus)
+            TextField(
+                value = annus,
+                onValueChange = { annus = it },
+                modifier = Modifier.width(85.dp),
+                textStyle = TextStyle(textAlign = TextAlign.Center),
+                singleLine = true,
+                colors = textFieldColours,
+            )
         }
     }
 }
