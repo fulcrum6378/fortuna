ad47e53fa080cf9e66968e7482b876e2c9ddb1bb
Mahdi Parastesh <fulcrum1378@gmail.com>
2026-02-04 21:01:01 +0330

    2026.02.04 - 1: web app

--- android-view/src/assets/web_app/script.js
+++ android-view/src/assets/web_app/script.js
@@ -22,16 +22,16 @@ $.ajax({
         $('#nav-month span').click(function() {
             if (month == $(this).index() + 1) return;
             month = $(this).index() + 1;
-            luna(false);
+            get_luna(false);
         });
 
         year = calendar.thisYear;
         month = calendar.thisMonth;
-        luna(true);
+        get_luna(true);
     },
 });
 
-function luna(yearChanged) {
+function get_luna(yearChanged) {
 
     // Year Navigation Menu
     if (yearChanged) {
@@ -52,7 +52,7 @@ function luna(yearChanged) {
                 year -= yearRange - inc;
             else
                 year += inc - yearRange;
-            luna(true);
+            get_luna(true);
         });
     }
 
@@ -76,22 +76,26 @@ function luna(yearChanged) {
                 let clsScore = rawScore != null ? rawScore : _luna.defaultScore;
                 let clsMood = '';
                 let clsLevel = '';
+                let clsToday = '';
                 if (clsScore > 0) {
                     clsMood = 'pleasant ';
-                    clsLevel = 'lv' + clsScore.toString().replace('.', '_');
+                    clsLevel = 'lv' + clsScore.toString().replace('.', '_') + ' ';
                 } else if (clsScore < 0) {
                     clsMood = 'painful ';
-                    clsLevel = 'lv' + clsScore.toString().substring(1).replace('.', '_');
+                    clsLevel = 'lv' + clsScore.toString().substring(1).replace('.', '_') + ' ';
                 } else {
-                    clsMood = 'mediocre';
+                    clsMood = 'mediocre ';
                 }
+                if (year == calendar.thisYear && month == calendar.thisMonth &&
+                        d == calendar.thisDay - 1)
+                    clsToday = 'today ';
 
                 let visScore = (rawScore != null ? rawScore
                         : (!isFuture && _luna.defaultScore != null ? 'c. ' + _luna.defaultScore
                         : '?'));
                 let clsScoreNonImportant = _luna.scores[d] == null ? ' class="non-important"' : '';
 
-                $('#grid').append('<div class="dies ' + clsMood + clsLevel + '">' +
+                $('#grid').append('<div class="dies ' + clsMood + clsLevel + clsToday + '">' +
                         '<p>' + 
                         '<span>' + (_luna.emojis[d] ? _luna.emojis[d] : '') + '</span>' +
                         (_luna.verba[d] === 1 ? '<svg><use href="#ic_verbum" /></svg>' : '') +
@@ -108,23 +112,29 @@ function luna(yearChanged) {
             });
 
             day = 0;
-            $('#score option:eq(' + scoreToIndex(_luna.defaultScore) + ')').prop('selected', true);
+            /*$('#score option:eq(' + scoreToIndex(_luna.defaultScore) + ')').prop('selected', true);
             $('#emoji').val(_luna.defaultEmoji);
-            $('#verbum').val(_luna.defaultVerbum);
+            $('#verbum').val(_luna.defaultVerbum);*/
+            dies_success({score: _luna.defaultScore, emoji: _luna.defaultEmoji, verbum: defaultVerbum});
         },
     });
 }
 
 function dies() {
+    $('.dies[selected]').removeAttr('selected');
+    if (day > 0) $('.dies:nth-child(' + day + ')').prop('selected', true);
+
     $.ajax({
         url: 'dies?year=' + year + '&month=' + month + '&day=' + day,
         dataType: 'json',
-        success: function(_dies) {
-            $('#score option:eq(' + scoreToIndex(_dies.score) + ')').prop('selected', true);
-            $('#emoji').val(_dies.emoji);
-            $('#verbum').val(_dies.verbum);
-        },
+        success: dies_success,
     });
+}
+
+function dies_success(_dies) {
+    $('#score option:eq(' + scoreToIndex(_dies.score) + ')').prop('selected', true);
+    $('#emoji').val(_dies.emoji);
+    $('#verbum').val(_dies.verbum);
 }
 
 function scoreToIndex(score) {
@@ -155,5 +165,7 @@ function scoreToIndex(score) {
             return 11;
         case -3.0:
             return 12;
+        default:
+            return 5;
     }
 }
